1c61aacdf8ddc96eac2ed6b7add559634076b7e9
eduardo martinez <eduardo.martinez@externalia.local>
2020-12-18 09:10:52 +0100

    use .DefaultIfEmpty() b4 linq .Aggregate

---
+++ Aggregate/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Company.Product.Aggregate
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var a = Array.Empty<int>()
+                .DefaultIfEmpty();
+            int max = a.Aggregate(
+              (acc, cur) => cur > acc ? cur : acc);
+            Console.WriteLine(max);
+        }
+    }
+}
