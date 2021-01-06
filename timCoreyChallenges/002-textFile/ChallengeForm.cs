116b9d44e0c53459eb6147cee8a5a609c19d9185
Edu M <>
2021-01-06 13:30:56 +0100

    Tim Corey TextFileChallenge

---
+++ timCoreyChallenges/002-textFile/ChallengeForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Forms;
+
+namespace TextFileChallenge
+{
+    public partial class ChallengeForm : Form
+    {
+        private const string DataSetFile = "AdvancedDataSet.csv"; //"AdvancedDataSet.csv"
+
+        private BindingList<UserModel> users = new BindingList<UserModel>();
+        private IUsersService usersService = new UsersService();
+
+        public ChallengeForm()
+        {
+            InitializeComponent();
+
+            LoadUsersFromFile(
+                Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+                   DataSetFile));
+
+            WireUpDropDown();
+        }
+
+        private void LoadUsersFromFile(string filePath)
+        {
+            foreach (var user in usersService.GetAllFromFile(filePath))
+            {
+                users.Add(user);
+            }
+        }
+
+        private void WireUpDropDown()
+        {
+            usersListBox.DataSource = users;
+            usersListBox.DisplayMember = nameof(UserModel.DisplayText);
+        }
+
+        private void addUserButton_Click(object sender, EventArgs e)
+        {
+            // TODO - validate railway-style
+
+            var user = new UserModel
+            {
+                FirstName = firstNameText.Text,
+                LastName = lastNameText.Text,
+                Age = (int)agePicker.Value, // TODO - unsafe
+                IsAlive = isAliveCheckbox.Checked
+            };
+            users.Add(user);
+        }
+
+        private void saveListButton_Click(object sender, EventArgs e)
+        {
+            usersService.SaveToFile(users, DataSetFile);
+        }
+    }
+}
---
+++ timCoreyChallenges/002-textFile/UsersService.cs
@@ -0,0 +1,29 @@
+﻿using CsvHelper;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace TextFileChallenge
+{
+    public class UsersService : IUsersService
+    {
+        public IReadOnlyCollection<UserModel> GetAllFromFile(string filePath)
+        {
+            using (var reader = new StreamReader(filePath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                return csv.GetRecords<UserModel>().ToList();
+            }
+        }
+
+        public void SaveToFile(IEnumerable<UserModel> users, string filePath)
+        {
+            using (var writer = new StreamWriter(filePath))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(users);
+            }
+        }
+    }
+}
