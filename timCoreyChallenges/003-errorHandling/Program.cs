e4b27f397ea0309bb31254639d2e9aaa54d19f3a
Edu M <>
2021-01-09 10:14:57 +0100

    tim corey error handling challenge - basic

---
+++ timCoreyChallenges/003-errorHandling/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using DemoLibrary;
+
+namespace ErrorHandlingChallenge
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            PaymentProcessor paymentProcessor = new PaymentProcessor();
+            for (int i = 0; i <= 10; i++)
+            {
+                try
+                {
+                    var result = paymentProcessor.MakePayment($"Demo{ i }", i);
+                    if (result == null)
+                    {
+                        Console.WriteLine($"Null value for item {i}", i);
+                    }
+
+                    Console.WriteLine(result.TransactionAmount);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine($"Payment skipped for payment with {i} items", i);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
