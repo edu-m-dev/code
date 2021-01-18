f23fac5992581f1e59b003e48fab074577c56c33
Edu M <>
2021-01-18 10:40:14 +0100

    tim corey refactoring challenge - IRepo partially implemented for dapper and EF

---
+++ timCoreyChallenges/004-refactoring/refactoring.bl/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace refactoring.bl
+{
+    public interface IUserRepository : IRepository<UserModel>
+    {
+        IReadOnlyCollection<UserModel> GetByFirstOrLastName(string name);
+    }
+}
