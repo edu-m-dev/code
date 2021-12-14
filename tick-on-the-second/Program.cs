a44ee3443e4c35d380c5b34557da5ac318f09a23
Edu M <>
2021-12-14 16:06:04 +0100

    tick-on-the-second initial commit

---
+++ tick-on-the-second/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Reactive.Concurrency;
+
+Action work = () => Console.WriteLine(DateTime.Now.ToLongTimeString());
+
+Scheduler.Default.Schedule(
+    // start in so many seconds
+    TimeSpan.FromMilliseconds(1000 - DateTime.Now.Millisecond), 
+    // then run every minute
+    () => Scheduler.Default.SchedulePeriodic(TimeSpan.FromSeconds(1), work));               
+
+Console.WriteLine("Press return to exit");
+Console.ReadLine();
