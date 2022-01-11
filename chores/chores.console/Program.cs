df1810e130ead80585f3cc62a49227150ad64b94
Edu M <>
2022-01-11 14:13:48 +0100

    original commit #7

---
+++ chores/chores.console/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using chores.bl.ef;
+
+using (var dbContext = new ChoresDbContext())
+
+    dbContext.Chores.ToList().ForEach(x =>
+        Console.WriteLine(x.Name));
