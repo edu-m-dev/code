f9de70b49a9a902556ae7bb1addd161203b55ddd
Edu M <>
2022-01-18 10:57:07 +0100

    wwi-hr initial commit

---
+++ wwi-hr/wwi-hr.console/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using wwi.hr.EF;
+
+namespace wwi.hr
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            using var dbContext = new WwiContext();
+            var people = dbContext.People;
+            foreach (var person in people)
+            {
+                Console.WriteLine($"{ person.FullName}");
+            }
+        }
+    }
+}
