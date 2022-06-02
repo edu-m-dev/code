f7bf7b7c01ab871bbdfe91dd672fbaedeb17ccce
Edu M <>
2022-06-02 14:12:03 +0200

    Return Collection Async #26

---
+++ async/Program.cs
@@ -0,0 +1,24 @@
+﻿static async Task<string[]> ReturnCollectionAsync()
+{
+    List<Task<string>> tasks = new List<Task<string>>();
+    for (int i = 0; i < 5; i++)
+    {
+        var currentTask = GetStringAsync();
+        tasks.Add(currentTask);
+    }
+
+    return await Task.WhenAll(tasks);
+}
+
+static async Task<string> GetStringAsync()
+{
+    var rnd = new Random();
+    return $"Result string {rnd.Next()}";
+}
+
+var results = await ReturnCollectionAsync();
+foreach (var result in results)
+{
+    Console.WriteLine(result);
+}
+Console.ReadLine();
