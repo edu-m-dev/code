5f58dfaeaf80ce6f08006ef628701666f2e6ce8d
Edu M <>
2023-05-05 11:44:13 +0200

    start with provided skeleton, all tests fail

---
+++ equality/equality.bl/Vehicle.cs
@@ -0,0 +1,21 @@
+﻿namespace equality.bl;
+public class Vehicle
+{
+
+    private string manufacturer;
+    private string model;
+    private int productionYear;
+
+    public Vehicle(string manufacturer, string model, int productionYear)
+    {
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.productionYear = productionYear;
+    }
+
+    public string GetLabel()
+    {
+        return string.Format("{0} {1}", this.manufacturer, this.model);
+    }
+
+}
