bd1d39f744188721dd7b773ce61be5b16b2eabbb
Edu M <>
2023-05-05 11:44:13 +0200

    start with provided skeleton, all tests fail #32

---
+++ equality/equality.console/Program.cs
@@ -0,0 +1,36 @@
+﻿using equality.bl;
+
+IEnumerable<Vehicle> vehicles = LoadVehicles();
+
+ReportVehicles(vehicles);
+
+Console.ReadLine();
+
+static IEnumerable<Vehicle> LoadVehicles()
+{
+    return new List<Vehicle>()
+            {
+                new Vehicle("Fiat", "Bravo",1995),
+                new Vehicle("Opel", "Meriva",2004),
+                new Vehicle("Renault", "Logan",2006),
+                new Vehicle("Opel", "Speedster",2002),
+                new Vehicle("Renault", "Clio",1996),
+                new Vehicle("Fiat", "Bravo",1997),
+                new Vehicle("Opel", "Meriva",2007),
+                new Vehicle("Renault", "Fluence",2011),
+                new Vehicle("Fiat", "Seicento",1998),
+                new Vehicle("Renault", "Clio",2002)
+            };
+}
+
+static void ReportVehicles(IEnumerable<Vehicle> vehicles)
+{
+
+    var query =
+        (from v in vehicles
+         group v by v into g
+         select new { Vehicle = g.Key.GetLabel(), Count = g.Count() });
+
+    foreach (var record in query)
+        Console.WriteLine("{0,-17} x {1}", record.Vehicle, record.Count);
+}
---
+++ equality/equality.tests/Vehicle_UnitTests.cs
@@ -0,0 +1,38 @@
+﻿using equality.bl;
+
+namespace equality.tests;
+
+[TestClass]
+public class Vehicle_UnitTests
+{
+    [TestMethod]
+    public void Equality_SameManufacturerAndModelDistinctYear()
+    {
+        Vehicle vehicle1 = new Vehicle("Renault", "Fluence", 2011);
+        Vehicle vehicle2 = new Vehicle("Renault", "Fluence", 2012);
+        EqualityTests.TestEqualObjects(vehicle1, vehicle2);
+    }
+
+    [TestMethod]
+    public void Inequality_SameManufacturerDistinctModel()
+    {
+        Vehicle vehicle1 = new Vehicle("Renault", "Fluence", 2011);
+        Vehicle vehicle2 = new Vehicle("Renault", "Clio", 2011);
+        EqualityTests.TestUnequalObjects(vehicle1, vehicle2);
+    }
+
+    [TestMethod]
+    public void Inequality_DistinctManufacturerSameModel()
+    {
+        Vehicle vehicle1 = new Vehicle("Opel", "Fluence", 2011);
+        Vehicle vehicle2 = new Vehicle("Renault", "Fluence", 2011);
+        EqualityTests.TestUnequalObjects(vehicle1, vehicle2);
+    }
+
+    [TestMethod]
+    public void Inequality_ComparingWithNull()
+    {
+        Vehicle vehicle = new Vehicle("Renault", "Fluence", 2011);
+        EqualityTests.TestAgainstNull(vehicle);
+    }
+}
