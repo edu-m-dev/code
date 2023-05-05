3cd820519e1d7e950f37ce0188e81d32c4049680
Edu M <>
2023-05-05 14:56:23 +0200

    implemented non-strongly-typed Equals

--- equality/equality.bl/Vehicle.cs
+++ equality/equality.bl/Vehicle.cs
@@ -1,21 +1,40 @@
 ﻿namespace equality.bl;
 public class Vehicle
 {
-
-    private string manufacturer;
-    private string model;
-    private int productionYear;
+    private string _manufacturer;
+    private string _model;
+    private int _productionYear;
 
     public Vehicle(string manufacturer, string model, int productionYear)
     {
-        this.manufacturer = manufacturer;
-        this.model = model;
-        this.productionYear = productionYear;
+        _manufacturer = manufacturer;
+        _model = model;
+        _productionYear = productionYear;
     }
 
     public string GetLabel()
     {
-        return string.Format("{0} {1}", this.manufacturer, this.model);
+        return string.Format("{0} {1}", _manufacturer, _model);
     }
 
+    public override bool Equals(object? obj) => obj switch
+    {
+        Vehicle v when _manufacturer.Equals(v._manufacturer) && _model.Equals(v._model) => true,
+        _ => false,
+    };
+
+    public override int GetHashCode()
+    {
+        return _manufacturer.GetHashCode() ^ _model.GetHashCode();
+    }
+
+    public static bool operator ==(Vehicle? x, Vehicle? y) 
+    {
+        return x?.Equals(y) ?? false;
+    }
+
+    public static bool operator !=(Vehicle? x, Vehicle? y)
+    {
+        return !x?.Equals(y) ?? true;
+    }
 }
