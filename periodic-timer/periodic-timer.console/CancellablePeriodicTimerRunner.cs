30263fcf33578abed8b6ee8a919ccd417be813cb
Edu M <edu@DIRTY_RAT>
2023-05-19 18:15:08 +0200

    implemented a periodic timer example with cancellation #33

---
+++ periodic-timer/periodic-timer.console/CancellablePeriodicTimerRunner.cs
@@ -0,0 +1,31 @@
+﻿namespace periodic_timer.console
+{
+    public class CancellablePeriodicTimer
+    {
+        private readonly string _name;
+        private readonly TimeSpan _timeSpan;
+        private readonly CancellationToken _cancellationToken;
+
+        public CancellablePeriodicTimer(string name, TimeSpan timeSpan, CancellationToken cancellationToken)
+        {
+            _name = name;
+            _timeSpan = timeSpan;
+            _cancellationToken = cancellationToken;
+        }
+        public async Task StartAsync()
+        {
+            using PeriodicTimer timer = new(_timeSpan);
+            try
+            {
+                while (await timer.WaitForNextTickAsync(_cancellationToken))
+                {
+                    Console.WriteLine($"{DateTime.Now} from {_name}");
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine($"timer {_name} stopped");
+            }
+        }
+    }
+}
---
+++ periodic-timer/periodic-timer.console/Program.cs
@@ -0,0 +1,16 @@
+﻿using periodic_timer.console;
+
+var tasks = Enumerable.Range(1, 10)
+        .Select(x =>
+         {
+             var cts = new CancellationTokenSource();
+             cts.CancelAfter(TimeSpan.FromSeconds(3));
+             return (cts, timer: new CancellablePeriodicTimer(x.ToString(), TimeSpan.FromSeconds(1), cts.Token));
+         })
+    .Select(async t =>
+{
+    await t.timer.StartAsync();
+});
+
+await Task.WhenAll(tasks);
+Console.WriteLine("all timers stopped");
