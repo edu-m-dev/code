dd78c3cbf1fa8e98549bf59a88089204287e7916
Edu M <edu@DIRTY_RAT>
2023-05-22 16:26:57 +0200

    class vs record equality #32

---
+++ equality/equality.bl/VehicleRecord.cs
@@ -0,0 +1,18 @@
+﻿namespace equality.bl;
+public record VehicleRecord
+{
+    private string _manufacturer;
+    private string _model;
+    private int _productionYear;
+
+    public VehicleRecord(string manufacturer, string model, int productionYear)
+    {
+        _manufacturer = manufacturer;
+        _model = model;
+        _productionYear = productionYear;
+    }
+    public string GetLabel()
+    {
+        return string.Format("{0} {1}", _manufacturer, _model);
+    }
+}
---
+++ equality/equality.tests/VehicleRecord_UnitTests.cs
@@ -0,0 +1,46 @@
+﻿using equality.bl;
+
+namespace equality.tests;
+
+[TestClass]
+public class VehicleRecord_UnitTests
+{
+    [TestMethod]
+    public void Equality_SameManufacturerAndModelAndYear()
+    {
+        VehicleRecord vehicle1 = new VehicleRecord("Renault", "Fluence", 2011);
+        VehicleRecord vehicle2 = new VehicleRecord("Renault", "Fluence", 2011);
+        EqualityTests.TestEqualObjects(vehicle1, vehicle2);
+    }
+
+    [TestMethod]
+    public void Inequality_SameManufacturerAndModelDistinctYear()
+    {
+        VehicleRecord vehicle1 = new VehicleRecord("Renault", "Fluence", 2011);
+        VehicleRecord vehicle2 = new VehicleRecord("Renault", "Fluence", 2012);
+        EqualityTests.TestUnequalObjects(vehicle1, vehicle2);
+    }
+
+    [TestMethod]
+    public void Inequality_SameManufacturerDistinctModel()
+    {
+        VehicleRecord vehicle1 = new VehicleRecord("Renault", "Fluence", 2011);
+        VehicleRecord vehicle2 = new VehicleRecord("Renault", "Clio", 2011);
+        EqualityTests.TestUnequalObjects(vehicle1, vehicle2);
+    }
+
+    [TestMethod]
+    public void Inequality_DistinctManufacturerSameModel()
+    {
+        VehicleRecord vehicle1 = new VehicleRecord("Opel", "Fluence", 2011);
+        VehicleRecord vehicle2 = new VehicleRecord("Renault", "Fluence", 2011);
+        EqualityTests.TestUnequalObjects(vehicle1, vehicle2);
+    }
+
+    [TestMethod]
+    public void Inequality_ComparingWithNull()
+    {
+        VehicleRecord vehicle = new VehicleRecord("Renault", "Fluence", 2011);
+        EqualityTests.TestAgainstNull(vehicle);
+    }
+}
