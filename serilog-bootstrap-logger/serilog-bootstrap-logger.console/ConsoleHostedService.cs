2dc6b3bb20c489ceeb5825f642a72adfbd90f2fc
Edu M <edu@DIRTY_RAT>
2023-05-23 16:56:32 +0200

    implemented serilog bootstrap logger infra + generic host with IHostedService worker accessing application lifecycle service #36

---
+++ serilog-bootstrap-logger/serilog-bootstrap-logger.console/ConsoleHostedService.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+public class ConsoleHostedService : IHostedService
+{
+    private readonly ILogger<ConsoleHostedService> _logger;
+    private readonly IHostApplicationLifetime _hostApplicationLifetime;
+
+    public ConsoleHostedService(ILogger<ConsoleHostedService> logger, IHostApplicationLifetime hostApplicationLifetime)
+    {
+        _logger = logger;
+        _hostApplicationLifetime = hostApplicationLifetime;
+    }
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("hosted service has started");
+        _hostApplicationLifetime.ApplicationStarted.Register(() =>
+        {
+            _logger.LogInformation("application has started");
+
+            Task.Run(async () =>
+            {
+                try
+                {
+                    _logger.LogInformation("you've got 5s to exit with ctrl+c");
+                    await Task.Delay(5000, cancellationToken);
+                    _logger.LogInformation("manually throwing from thread");
+                    throw new Exception("manual exception from thread");
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, ex.Message);
+                }
+                finally
+                {
+                    _hostApplicationLifetime.StopApplication();
+                }
+            }, cancellationToken);
+        });
+
+        _hostApplicationLifetime.ApplicationStopped.Register(() =>
+        {
+            _logger.LogInformation("application has stopped");
+        });
+
+        _hostApplicationLifetime.ApplicationStopping.Register(() =>
+        {
+            _logger.LogInformation("application is stopping");
+        });
+
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("hosted service has stopped");
+        return Task.CompletedTask;
+    }
+}
---
+++ serilog-bootstrap-logger/serilog-bootstrap-logger.console/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+           .WriteTo.Console()
+           .CreateBootstrapLogger();
+
+try
+{
+    await Host.CreateDefaultBuilder(args)
+        .UseSerilog((context, services, configuration) => configuration
+                .WriteTo.Console()
+                // But, we have access to configuration and services from the host
+                .ReadFrom.Configuration(context.Configuration)
+                .ReadFrom.Services(services))
+        .ConfigureServices((context, services) =>
+        {
+            services.AddHostedService<ConsoleHostedService>();
+        })
+        .RunConsoleAsync();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "An unhandled exception occured during bootstrapping");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
