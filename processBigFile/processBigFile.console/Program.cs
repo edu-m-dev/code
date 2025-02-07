ab8e35824b045ba147a61e5a0865f6ffdf50e7c7
Edu M <edu@DIRTY_RAT>
2025-02-07 07:16:24 +0100

    remove blank files and show the rest in an async-stream fashion

--- processBigFile/processBigFile.console/Program.cs
+++ processBigFile/processBigFile.console/Program.cs
@@ -2,8 +2,10 @@
 
 var cts = new CancellationTokenSource();
 
-var lines = await new ReadableFile("./file.txt").ReadAllLinesAsync(cts.Token);
-foreach (var line in lines)
+var lines = (await new ReadableFile("./file.txt")
+    .ReadAllLinesAsync(cts.Token))
+    .RemoveBlankLines(cts.Token);
+await foreach (var line in lines)
 {
     Console.WriteLine(line);
 }
---
+++ processBigFile/processBigFile.console/ReadableFileExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace processBigFile.console;
+
+public static class ReadableFileExtensions
+{
+    public static async IAsyncEnumerable<string> RemoveBlankLines(this IEnumerable<string> lines, CancellationToken token)
+    {
+        foreach (var line in lines)
+        {
+            token.ThrowIfCancellationRequested();
+            if (!string.IsNullOrWhiteSpace(line))
+            {
+                await Task.Delay(1000, token);
+                yield return line;
+            }
+        }
+    }
+}
