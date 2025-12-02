55be798af919dcb5175fc8d53718a911a27ea919
edu-m-dev <8697406+edu-m-dev@users.noreply.github.com>
2025-12-02 18:14:41 +0100

    created enigma webapp

---
+++ enigma/enigma.webapp/Program.cs
@@ -0,0 +1,25 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApi();
+
+builder.Services.AddOpenApiDocument(config =>
+{
+    config.Title = "enigma api";
+    config.Version = "v1";
+});
+
+
+var app = builder.Build();
+
+//if (app.Environment.IsDevelopment())
+//{
+app.UseDeveloperExceptionPage();
+app.UseOpenApi();     // Serves /swagger/v1/swagger.json
+app.UseSwaggerUi();  // Serves Swagger UI at /swagger
+//}
+
+app.MapControllers();
+
+app.Run();
