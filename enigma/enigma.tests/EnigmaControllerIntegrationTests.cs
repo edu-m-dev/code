1e9863118ce4d9ece6a6a7228570c86df7272d1e
Edu M <8697406+edu-m-dev@users.noreply.github.com>
2025-12-03 12:40:10 +0100

    created controller tests (#67)
    * added controller unit tests
    
    * added controller integration tests

---
+++ enigma/enigma.tests/EnigmaControllerIntegrationTests.cs
@@ -0,0 +1,85 @@
+﻿using System.Net.Http.Json;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using NUnit.Framework;
+
+namespace enigma.tests;
+
+public class EnigmaControllerIntegrationTests
+{
+    private WebApplicationFactory<Program>? _factory;
+
+    private static readonly string[] _testRotors = new[]
+    {
+        "BDFHJLCPRTXVZNYEIWGAKMUSQO",
+        "AJDKSIRUXBLHWTMCQGZNPYFVOE",
+        "EKMFLGDQVZNTOWYHXUSPAIBRCJ"
+    };
+
+    [SetUp]
+    public void SetUp()
+    {
+        _factory = new WebApplicationFactory<Program>();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _factory?.Dispose();
+    }
+
+    [Test]
+    public async Task PostEncode_Returns200WithResult()
+    {
+        var client = _factory!.CreateClient();
+        var req = new { Message = "HELLO", Shift = 1, Rotors = _testRotors };
+        var resp = await client.PostAsJsonAsync("/api/enigma/encode", req);
+        resp.EnsureSuccessStatusCode();
+
+        var content = await resp.Content.ReadFromJsonAsync<System.Text.Json.JsonElement>();
+        content.GetProperty("result").GetString().Should().NotBeNullOrEmpty();
+    }
+
+    [Test]
+    public async Task PostDecode_Returns200WithResult()
+    {
+        var client = _factory!.CreateClient();
+        var req = new { Message = "HELLO", Shift = 1, Rotors = _testRotors };
+        var resp = await client.PostAsJsonAsync("/api/enigma/decode", req);
+        resp.EnsureSuccessStatusCode();
+
+        var content = await resp.Content.ReadFromJsonAsync<System.Text.Json.JsonElement>();
+        content.GetProperty("result").GetString().Should().NotBeNullOrEmpty();
+    }
+
+    [Test]
+    public async Task PostEncode_InvalidMessage_ReturnsBadRequest()
+    {
+        var client = _factory!.CreateClient();
+        var req = new { Message = "", Shift = 1, Rotors = _testRotors };
+        var resp = await client.PostAsJsonAsync("/api/enigma/encode", req);
+        resp.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
+    }
+
+    [Test]
+    public async Task Encode_Decode()
+    {
+        var client = _factory!.CreateClient();
+        var req = new { Message = "TALESOFTHEUNEXPECTED", Shift = 1, Rotors = _testRotors };
+        var encodedResp = await client.PostAsJsonAsync("/api/enigma/encode", req);
+        encodedResp.EnsureSuccessStatusCode();
+
+        var encodedContent = await encodedResp.Content.ReadFromJsonAsync<System.Text.Json.JsonElement>();
+        var encoded = encodedContent.GetProperty("result").GetString();
+        encoded.Should().NotBeNullOrEmpty();
+
+        // Now decode the message
+        var decodeResp = await client.PostAsJsonAsync("/api/enigma/decode", new { Message = encoded, Shift = 1, Rotors = _testRotors });
+        decodeResp.EnsureSuccessStatusCode();
+
+        var decodeContent = await decodeResp.Content.ReadFromJsonAsync<System.Text.Json.JsonElement>();
+        var decoded = decodeContent.GetProperty("result").GetString();
+        decoded.Should().NotBeNullOrEmpty();
+        decoded.Should().Be(req.Message);
+    }
+}
---
+++ enigma/enigma.tests/EnigmaControllerUnitTests.cs
@@ -0,0 +1,117 @@
+﻿using System.Reflection;
+using enigma.webapp.Controllers;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+
+namespace enigma.tests;
+
+public class EnigmaControllerUnitTests
+{
+    [Test]
+    public void Encode_ValidRequest_ReturnsOkWithResult()
+    {
+        var req = new EnigmaController.EnigmaRequest("HELLO", 1, null);
+        var controller = new EnigmaController();
+
+        var actionResult = controller.Encode(req);
+
+        actionResult.Should().BeOfType<OkObjectResult>();
+        var ok = (OkObjectResult)actionResult;
+        ok.Value.Should().NotBeNull();
+
+        var prop = ok.Value!.GetType().GetProperty("result", BindingFlags.Public | BindingFlags.Instance);
+        prop.Should().NotBeNull();
+        var resultString = prop!.GetValue(ok.Value) as string;
+        resultString.Should().NotBeNullOrEmpty();
+    }
+
+    [Test]
+    public void Decode_ValidRequest_ReturnsOkWithResult()
+    {
+        var req = new EnigmaController.EnigmaRequest("HELLO", 2, null);
+        var controller = new EnigmaController();
+
+        var actionResult = controller.Decode(req);
+
+        actionResult.Should().BeOfType<OkObjectResult>();
+        var ok = (OkObjectResult)actionResult;
+        var prop = ok.Value!.GetType().GetProperty("result", BindingFlags.Public | BindingFlags.Instance);
+        var resultString = prop!.GetValue(ok.Value) as string;
+        resultString.Should().NotBeNullOrEmpty();
+    }
+
+    [Test]
+    public void Encode_NullRequest_ReturnsBadRequest()
+    {
+        var controller = new EnigmaController();
+        // call with null at runtime; use the null-forgiving operator to satisfy nullable analysis
+        var actionResult = controller.Encode(null!);
+        actionResult.Should().BeOfType<BadRequestObjectResult>();
+    }
+
+    [Test]
+    public void Decode_NullRequest_ReturnsBadRequest()
+    {
+        var controller = new EnigmaController();
+        var actionResult = controller.Decode(null!);
+        actionResult.Should().BeOfType<BadRequestObjectResult>();
+    }
+
+    [Test]
+    public void Encode_EmptyMessage_ReturnsBadRequest()
+    {
+        var req = new EnigmaController.EnigmaRequest(string.Empty, 1, null);
+        var controller = new EnigmaController();
+        var actionResult = controller.Encode(req);
+        actionResult.Should().BeOfType<BadRequestObjectResult>();
+    }
+
+    [Test]
+    public void Decode_EmptyMessage_ReturnsBadRequest()
+    {
+        var req = new EnigmaController.EnigmaRequest(string.Empty, 1, null);
+        var controller = new EnigmaController();
+        var actionResult = controller.Decode(req);
+        actionResult.Should().BeOfType<BadRequestObjectResult>();
+    }
+
+    [Test]
+    public void Encode_LowercaseMessage_ReturnsOk()
+    {
+        var req = new EnigmaController.EnigmaRequest("hello", 1, null);
+        var controller = new EnigmaController();
+
+        var actionResult = controller.Encode(req);
+        actionResult.Should().BeOfType<OkObjectResult>();
+    }
+
+    [Test]
+    public void Encode_InvalidCharacters_ThrowsArgumentException()
+    {
+        var req = new EnigmaController.EnigmaRequest("HELLO!", 1, null);
+        var controller = new EnigmaController();
+
+        FluentActions.Invoking(() => controller.Encode(req)).Should().Throw<ArgumentException>();
+    }
+
+    [Test]
+    public void Decode_InvalidCharacters_ThrowsArgumentException()
+    {
+        var req = new EnigmaController.EnigmaRequest("123", 1, null);
+        var controller = new EnigmaController();
+
+        FluentActions.Invoking(() => controller.Decode(req)).Should().Throw<ArgumentException>();
+    }
+
+    [Test]
+    public void Encode_WithCustomRotors_ReturnsOk()
+    {
+        var customRotors = new[] { "EKMFLGDQVZNTOWYHXUSPAIBRCJ" };
+        var req = new EnigmaController.EnigmaRequest("TEST", 3, customRotors);
+        var controller = new EnigmaController();
+
+        var actionResult = controller.Encode(req);
+        actionResult.Should().BeOfType<OkObjectResult>();
+    }
+}
