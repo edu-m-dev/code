00b163dbe6ca5442c569e408ec4f51f019fbd1c5
edu-m-dev <8697406+edu-m-dev@users.noreply.github.com>
2025-12-10 10:17:12 +0100

    modernized

--- async-enumerable/async-enumerable.console/AuthorService.cs
+++ async-enumerable/async-enumerable.console/AuthorService.cs
@@ -1,15 +1,15 @@
 ﻿public class AuthorService
 {
-    Author[] authors = new Author[]{
+    Author[] authors = [
         new Author("steven","hawkins", 10),
         new Author("steve","jobs", 1),
         new Author("rosemund","pilcher", 100)
-    };
+    ];
     public async IAsyncEnumerable<Author> GetAuthorsAsync()
     {
         foreach (Author author in authors)
         {
-            await Task.Delay(TimeSpan.FromSeconds(2));
+            await Task.Delay(TimeSpan.FromSeconds(1));
             yield return author;
         }
     }
