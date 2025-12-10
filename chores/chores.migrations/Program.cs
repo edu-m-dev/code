25c1a45e9a4a701f7e73e33a1d15581de906fdd7
edu-m-dev <8697406+edu-m-dev@users.noreply.github.com>
2025-12-10 12:01:32 +0100

    initial migration

---
+++ chores/chores.migrations/Program.cs
@@ -0,0 +1,24 @@
+﻿using chores.bl.ef;
+using chores.migrations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration((context, config) =>
+    {
+        config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+    })
+    .ConfigureServices((context, services) =>
+    {
+        var configuration = context.Configuration;
+        var connection = configuration.GetConnectionString("chores") ?? "data source=chores.db";
+        services.AddDbContext<ChoresDbContext>(
+            options => options.UseSqlite(connection, sqlOptions =>
+                sqlOptions.MigrationsAssembly("chores.migrations")));
+        services.AddHostedService<MigrationHostedService>();
+    })
+    .Build();
+
+await host.RunAsync();
