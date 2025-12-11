6b0f49d1d760195a5627ca89e32b54e7deb21a79
edu-m-dev <8697406+edu-m-dev@users.noreply.github.com>
2025-12-11 12:25:49 +0100

    bug - in-memory db cannot win against sqlite

---
+++ chores/chores.tests/integration/CustomWebApplicationFactory.cs
@@ -0,0 +1,32 @@
+﻿using chores.bl.ef;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace chores.tests.integration;
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            // Remove all ChoresDbContext registrations
+            var toRemove = services
+                .Where(d => d.ServiceType == typeof(DbContextOptions<ChoresDbContext>)
+                         || d.ServiceType == typeof(ChoresDbContext))
+                .ToList();
+
+            foreach (var d in toRemove)
+                services.Remove(d);
+
+            // Register InMemory provider
+            services.AddDbContext<ChoresDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("ChoresTestDb");
+            });
+        });
+    }
+}
