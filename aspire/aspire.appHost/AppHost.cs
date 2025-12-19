434692942feb65af81a10dc2f74b5f21db48053c
edu-m-dev <8697406+edu-m-dev@users.noreply.github.com>
2025-12-19 07:27:19 +0100

    added aspire

--- aspire/aspire.appHost/AppHost.cs
+++ aspire/aspire.appHost/AppHost.cs
@@ -1,5 +1,15 @@
 ﻿var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.chores_webapi>("chores-webapi");
+// the string you pass to .AddDatabase("chores") in AppHost must match the name you use in GetConnectionString("chores").
+var sql = builder.AddSqlServer("chores-sqlserver")
+                 .AddDatabase("chores");
+
+var migrations = builder.AddProject<Projects.chores_migrations>("chores-migrations")
+    .WithReference(sql)
+    .WaitFor(sql);
+
+builder.AddProject<Projects.chores_webapi>("chores-webapi")
+    .WithReference(sql)
+    .WaitFor(migrations);
 
 builder.Build().Run();
