80a794efabf50ff1623b1bee5a1f594b0d59bacc
edu-m-dev <8697406+edu-m-dev@users.noreply.github.com>
2025-12-20 16:03:46 +0100

    added in-memory distributed cache to tests

--- chores/chores.webapi.tests/integration/CustomWebApplicationFactory.cs
+++ chores/chores.webapi.tests/integration/CustomWebApplicationFactory.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace chores.webapi.tests.integration;
 
@@ -9,5 +11,20 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
     {
         // Set environment BEFORE configuring services
         builder.UseEnvironment("Testing");
+
+        builder.ConfigureServices(services =>
+        {
+            // Remove any existing IDistributedCache (Redis) registration
+            var existingCache = services
+                .SingleOrDefault(d => d.ServiceType == typeof(IDistributedCache));
+            if (existingCache != null)
+            {
+                services.Remove(existingCache);
+            }
+
+            // Replace with in-memory cache for tests
+            services.AddDistributedMemoryCache();
+        });
+
     }
 }
