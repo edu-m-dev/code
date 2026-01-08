61356a1e038edc5d5ac8b09c583bf15f7ca4c772
edu-m-dev <8697406+edu-m-dev@users.noreply.github.com>
2026-01-08 09:23:02 +0100

    added woman-first combinations

--- switch-for-hierarchy/Program.cs
+++ switch-for-hierarchy/Program.cs
@@ -1,5 +1,5 @@
-﻿var man = new Person("Eduardo", new string[] { "Martinez de Salinas", "Vazquez" });
-var woman = new Person("Laetitia", new string[] { "Ponsar", "Treca" });
+﻿var man = new Person("man_name", new string[] { "man_surname_1", "man_surname_2" });
+var woman = new Person("woman_name", new string[] { "woman_surname_1", "woman_surname_2" });
 
 static string WomanSurnameDropped(Person man, Person woman) => man.LastNames.First();
 static string CombinedSurname(Person man, Person woman) => $"{man.LastNames.First()}-{woman.LastNames.First()}";
@@ -7,12 +7,17 @@ static string MultipleSurnames(Person man, Person woman) => string.Join(" ",
     man.LastNames.Zip(
     woman.LastNames,
     (x, y) => new[] { x, y })
-    .SelectMany(x=>x));
+    .SelectMany(x => x));
 
 // print different strategies for descendant surnames
+// man-first
 Print(man, woman, WomanSurnameDropped);
 Print(man, woman, CombinedSurname);
 Print(man, woman, MultipleSurnames);
+// woman-first
+Print(woman, man, WomanSurnameDropped);
+Print(woman, man, CombinedSurname);
+Print(woman, man, MultipleSurnames);
 
 static void Print(Person man, Person woman, Func<Person, Person, string> surnameStrategy)
 {
