9a9f0644a702d9b6668bd12be3d46a15ca57ad03
edu-m-dev <8697406+edu-m-dev@users.noreply.github.com>
2026-01-08 09:52:19 +0100

    fixed comments

--- tick-on-the-second/Program.cs
+++ tick-on-the-second/Program.cs
@@ -4,10 +4,10 @@ using System.Reactive.Concurrency;
 Action work = () => Console.WriteLine(DateTime.Now.ToLongTimeString());
 
 Scheduler.Default.Schedule(
-    // start in so many seconds
-    TimeSpan.FromMilliseconds(1000 - DateTime.Now.Millisecond), 
-    // then run every minute
-    () => Scheduler.Default.SchedulePeriodic(TimeSpan.FromSeconds(1), work));               
+    // start on the second
+    TimeSpan.FromMilliseconds(1000 - DateTime.Now.Millisecond),
+    // then run every second
+    () => Scheduler.Default.SchedulePeriodic(TimeSpan.FromSeconds(1), work));
 
 Console.WriteLine("Press return to exit");
 Console.ReadLine();
