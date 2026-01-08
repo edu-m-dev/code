71f6812841590286eb5fd13827d1cbd76f1775ad
edu-m-dev <8697406+edu-m-dev@users.noreply.github.com>
2026-01-08 09:16:39 +0100

    stagger timers

--- periodic-timer/periodic-timer.console/Program.cs
+++ periodic-timer/periodic-timer.console/Program.cs
@@ -4,7 +4,7 @@ var tasks = Enumerable.Range(1, 10)
         .Select(x =>
          {
              var cts = new CancellationTokenSource();
-             cts.CancelAfter(TimeSpan.FromSeconds(3));
+             cts.CancelAfter(TimeSpan.FromSeconds(x));
              return (cts, timer: new CancellablePeriodicTimer(x.ToString(), TimeSpan.FromSeconds(1), cts.Token));
          })
     .Select(async t =>
