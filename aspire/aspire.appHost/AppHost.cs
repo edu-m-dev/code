30acf1fe5b8e9eb1e5cdc906713e986684ae2e9d
edu-m-dev <8697406+edu-m-dev@users.noreply.github.com>
2026-01-09 10:25:47 +0100

    avoid aspire azure monitor connection exception

--- aspire/aspire.appHost/AppHost.cs
+++ aspire/aspire.appHost/AppHost.cs
@@ -5,12 +5,14 @@ var sql = builder.AddSqlServer("chores-sqlserver")
                  .AddDatabase("chores");
 
 var migrations = builder.AddProject<Projects.chores_migrations>("chores-migrations")
+    .WithEnvironment("AzureMonitor__ConnectionString", "InstrumentationKey=00000000-0000-0000-0000-000000000000;IngestionEndpoint=https://localhost/")
     .WithReference(sql)
     .WaitFor(sql);
 
 var redis = builder.AddRedis("chores-cache");
 
 builder.AddProject<Projects.chores_webapi>("chores-webapi")
+    .WithEnvironment("AzureMonitor__ConnectionString", "InstrumentationKey=00000000-0000-0000-0000-000000000000;IngestionEndpoint=https://localhost/")
     .WithReference(sql)
     .WaitFor(migrations)
     .WithReference(redis)
