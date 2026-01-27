b24d486a6b7a981b94ed245e9b5229493997e808
edu-m-dev <8697406+edu-m-dev@users.noreply.github.com>
2026-01-27 14:27:41 +0100

    fixed aspire webapi port

--- aspire/aspire.appHost/AppHost.cs
+++ aspire/aspire.appHost/AppHost.cs
@@ -1,4 +1,6 @@
-﻿var builder = DistributedApplication.CreateBuilder(args);
+﻿using Microsoft.Extensions.Configuration;
+
+var builder = DistributedApplication.CreateBuilder(args);
 
 // the string you pass to .AddDatabase("code") in AppHost must match the name you use in GetConnectionString("code").
 var sql = builder.AddSqlServer("code-sqlserver")
@@ -9,7 +11,14 @@ var migrations = builder.AddProject<Projects.chores_migrations>("chores-migratio
     .WithReference(sql)
     .WaitFor(sql);
 
+var webapiPort = builder.Configuration.GetValue<int>("ApiPort", 54196);
+
 builder.AddProject<Projects.chores_webapi>("chores-webapi")
+    .WithExternalHttpEndpoints() // keeps the default external endpoint
+    .WithHttpsEndpoint(
+        name: "fixed-https",       // must be unique
+        port: webapiPort          // your fixed port
+    )
     .WithEnvironment("AzureMonitor__ConnectionString", "InstrumentationKey=00000000-0000-0000-0000-000000000000;IngestionEndpoint=https://localhost/")
     .WithReference(sql)
     .WaitFor(migrations);
